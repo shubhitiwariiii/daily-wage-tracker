af53f730841758cc83fc24136c4fc24910d99cbd
Shubhi Tiwari <shubhitiwari0132@gmail.com>
2026-05-24 10:41:27 +0530

    Created Worker model schema

---
+++ backend/src/models/Worker.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const workerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    phone: {
+      type: String,
+      required: true,
+    },
+
+    dailyWage: {
+      type: Number,
+      required: true,
+    },
+
+    contractorId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Worker", workerSchema);
