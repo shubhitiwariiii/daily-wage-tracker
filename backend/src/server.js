64053c738434a1ff9ade6f4f976a79084793284c
Shubhi Tiwari <shubhitiwari0132@gmail.com>
2026-05-29 18:23:37 +0530

    Prepared project for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -11,7 +11,12 @@ const app = express();
 
 connectDB();
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+  }),
+);
 app.use(express.json());
 
 app.use("/api/workers", workerRoutes);
